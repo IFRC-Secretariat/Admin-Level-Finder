3524884cdaf445aba39fe5fe2f429d5bff4b8b59
Alex Howes <howesalexandra@gmail.com>
2023-07-13 16:49:22 +0100

    Restructure to give a list of points and loop through multiple points.

--- tests/test_find_points.py
+++ tests/test_find_points.py
@@ -10,36 +10,42 @@ class TestFindPoint(unittest.TestCase):
     Test finding some points and checking that the correct admin1 and admin2 names are returned.
     """
     def setUp(self):
-        self.iso3 = 'GBR'
-        self.crs = 'EPSG:4326'
-        self.point_4326 = (-3.272104, 51.083221)
-        self.utm_crs = 'EPSG:32642'
-        self.admin1 = "England"
-        self.admin2 = "Somerset"
+        self.points = [
+            {
+                "iso3": "GBR",
+                "crs": 'EPSG:4326',
+                "coords": (-3.272104, 51.083221),
+                "transform_crs": 'EPSG:32642',
+                "admin1": "England",
+                "admin2": "Somerset"
+            },
+        ]
 
 
     def test_find_point(self):
         """
         Test finding the results for a point in the UK.
         """
-        uk_finder = admin_levels_finder.AdminLevelsFinder(iso3=self.iso3, crs=self.crs)
-        results = uk_finder.find(self.point_4326, errors='raise')
-        self.assertEqual(results["NAME_1"], self.admin1)
-        self.assertEqual(results["NAME_2"], self.admin2)
+        for point in self.points:
+            finder = admin_levels_finder.AdminLevelsFinder(iso3=point['iso3'], crs=point['crs'])
+            results = finder.find(point['coords'], errors='raise')
+            self.assertEqual(results["NAME_1"], point['admin1'])
+            self.assertEqual(results["NAME_2"], point['admin2'])
 
 
     def test_convert_crs_point(self):
         """
         Test finding a point in a different CRS.
         """
-        # Convert the point to the UTM CRS
-        transformer = pyproj.Transformer.from_crs(pyproj.CRS(self.crs), 
-                                                  pyproj.CRS(self.utm_crs), 
-                                                  always_xy=True).transform
-        point_utm = transform(transformer, Point(self.point_4326))
-        utm_finder = admin_levels_finder.AdminLevelsFinder(iso3=self.iso3, crs=self.utm_crs)
-        results = utm_finder.find(list(point_utm.coords), errors='raise')
+        for point in self.points:
+            # Convert the point to a different CRS
+            transformer = pyproj.Transformer.from_crs(pyproj.CRS(point['crs']), 
+                                                      pyproj.CRS(point['transform_crs']), 
+                                                      always_xy=True).transform
+            point_transformed = transform(transformer, Point(point['coords']))
+            finder = admin_levels_finder.AdminLevelsFinder(iso3=point['iso3'], crs=point['transform_crs'])
+            results = finder.find(list(point_transformed.coords), errors='raise')
 
-        # Check the results
-        self.assertEqual(results["NAME_1"], self.admin1)
-        self.assertEqual(results["NAME_2"], self.admin2)
+            # Check the results
+            self.assertEqual(results["NAME_1"], point['admin1'])
+            self.assertEqual(results["NAME_2"], point['admin2'])
